be351634ed74f3cc37315b6e5cc0bec831f4898e
Amos <amoshuke@qq.com>
2023-07-16 17:44:56 +0800

    update: example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,133 +7,100 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Tilt Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.brown,
       ),
-      home: const Demo(),
+      home: const TiltDemo(),
     );
   }
 }
 
-class Demo extends StatelessWidget {
-  const Demo({super.key});
+class TiltDemo extends StatelessWidget {
+  const TiltDemo({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        child: ListView(
-          shrinkWrap: false,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 200,
-                  height: 300,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.grey.withOpacity(0.1),
-                        Colors.grey.withOpacity(0.1)
-                      ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                    ),
-                  ),
-                ),
-                Tilt(
-                  // initTilt: Offset(-1, -1),
-                  // direction: [
-                  //   TiltDirection.bottom,
-                  //   TiltDirection(0.1, 0.1),
-                  //   TiltDirection.left
-                  // ],
+        /// Tilt Demo
+        child: Tilt(
+          borderRadius: BorderRadius.circular(24),
+          tiltConfig: const TiltConfig(angle: 15),
+          lightConfig: const LightConfig(
+            minIntensity: 0.1,
+            maxIntensity: 0.3,
+          ),
+          shadowConfig: const ShadowConfig(
+            minIntensity: 0.05,
+            maxIntensity: 0.3,
+            spreadOrigin: -10,
+            minBlurRadius: 10,
+            maxBlurRadius: 20,
+          ),
+          child: const SizedBox(
+            width: 250,
+            height: 400,
+            child: MyHomePage(title: 'Flutter Tilt Demo'),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
-                  /// lightConfig: LightConfig(direction: LightDirection.top),
-                  // shadowConfig: ShadowConfig(
-                  //     direction: ShadowDirection.bottom, isReverse: false),
-                  borderRadius: BorderRadius.circular(10),
-                  child: Builder(
-                    builder: (context) {
-                      print('Demo Build');
-                      return SizedBox(
-                        width: 200,
-                        height: 300,
-                        child: Scaffold(
-                          backgroundColor: const Color(0x20777777),
-                          appBar:
-                              AppBar(title: const Text('Flutter Tilt Demo')),
-                          body: Center(
-                            child: Stack(
-                              alignment: AlignmentDirectional.center,
-                              children: [
-                                Tilt(
-                                  borderRadius: BorderRadius.circular(20),
-                                  child: Container(
-                                    width: 50,
-                                    height: 50,
-                                    decoration: BoxDecoration(
-                                      gradient: LinearGradient(
-                                        colors: [
-                                          Colors.grey.withOpacity(0.1),
-                                          Colors.grey.withOpacity(0.1)
-                                        ],
-                                        begin: Alignment.topLeft,
-                                        end: Alignment.bottomRight,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                Container(
-                  width: 200,
-                  height: 300,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.grey.withOpacity(0.1),
-                        Colors.grey.withOpacity(0.1)
-                      ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 200,
-                  height: 300,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.grey.withOpacity(0.1),
-                        Colors.grey.withOpacity(0.1)
-                      ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                    ),
-                  ),
-                ),
-              ],
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0x20777777),
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: const TextStyle(fontSize: 18),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+              style: TextStyle(fontSize: 10, color: Colors.white),
+            ),
+            Text(
+              '$_counter',
+              style: const TextStyle(fontSize: 20, color: Colors.white),
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
